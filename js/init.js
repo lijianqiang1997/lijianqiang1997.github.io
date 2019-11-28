de3cdf97a1ff562a5e2a38f01a245a572be322c1
unknown <1061879148@qq.com>
2019-11-28 16:04:50 +0800

    update

--- js/init.js
+++ js/init.js
@@ -5,19 +5,16 @@
                 length = imgEls.length,
                 imgEl,
                 elInfo,
-                arrTemp = imgEls;
+                arrTemp = imgEls.slice(0);
             if (imgEls.length > 0) {
                 for (; i < length; ++i) {
                     imgEl = imgEls[i];
-                    if (imgEl) {
-                        elInfo = imgEl.getBoundingClientRect();
-                        if (elInfo.top <= window.innerHeight) {
-                            imgEl.src = "http://lorempixel.com/1600/900?_t=" + (new Date() * Math.random())
-                            // 数组splice操作会影响循环
-                            arrTemp.splice(i, 1);
-                        }
+                    elInfo = imgEl.getBoundingClientRect();
+                    if (elInfo.top <= window.innerHeight) {
+                        imgEl.src = "http://lorempixel.com/1600/900?_t=" + (new Date() * Math.random())
+                        // 数组splice操作会影响循环
+                        arrTemp.splice(i, 1);
                     }
-
                 }
                 imgEls = arrTemp;
             }
@@ -27,7 +24,7 @@
             }
         }
     window.addEventListener("DOMContentLoaded", function () {
-        imgEls = Array.prototype.slice.call(document.querySelectorAll(".post-img"));
+        imgEls = Array.prototype.slice.call(document.querySelectorAll(".post-img-container .post-img"));
     })
     window.addEventListener("load", showImg)
     window.addEventListener("scroll", showImg)
