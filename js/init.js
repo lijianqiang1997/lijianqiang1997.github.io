57ce9c1254e7402825a05d430d6d0e702c66dc5a
unknown <1061879148@qq.com>
2019-11-28 14:16:41 +0800

    update

---
+++ js/init.js
@@ -0,0 +1,33 @@
+(function () {
+    var imgEls,
+        showImg = function () {
+            console.log(imgEls.length)
+            var i = 0,
+                length = imgEls.length,
+                imgEl,
+                elInfo,
+                arrTemp = imgEls;
+            if (imgEls.length > 0) {
+                for (; i < length; ++i) {
+                    imgEl = imgEls[i];
+                    console.log(i,imgEl);
+                    elInfo = imgEl.getBoundingClientRect();
+                    if (elInfo.top <= window.innerHeight) {
+                        imgEl.src = "http://lorempixel.com/1600/900?_t=" + (new Date() * Math.random())
+                        // 数组splice操作会影响循环
+                        arrTemp.splice(i,1);
+                    }
+                }
+                imgEls = arrTemp;
+            }
+
+            if (imgEls.length === 0) {
+                window.removeEventListener("scroll", showImg);
+            }
+        }
+    window.addEventListener("DOMContentLoaded", function () {
+        imgEls = Array.prototype.slice.call(document.querySelectorAll(".post-img"));
+    })
+    window.addEventListener("load", showImg)
+    window.addEventListener("scroll", showImg)
+})();
